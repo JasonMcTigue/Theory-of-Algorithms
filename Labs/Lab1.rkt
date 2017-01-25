4d7883c915d2cfbbee4d69ad1d95a2d1075c148b
Jason McTigue <jasonmctigue@live.ie>
2017-01-25 21:59:01 +0000

    Added Labs 1 and 2 and Q 1 and 2 of the problem sheet

---
+++ Labs/Lab1.rkt
@@ -0,0 +1,18 @@
+#lang racket
+;Different syntax
+
+(+ 3 5 8 1);adds numbers together
+
+(- 5 3 1);subtracts numbers order matters
+
+(* 5 3)
+
+(-(/ (+ 5 3) 2)2);To do muliple things at one use brackets.
+
+'(1 2 3_a b)
+
+(define plusone (lambda (x y) (+ 1 x y)));X + 1 eg 300 + 20 = 321
+
+(plusone 300 20)
+
+;Check out Project oiler.
